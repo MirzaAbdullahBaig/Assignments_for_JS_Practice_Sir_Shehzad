ab248dcf48ca5a744be0c1aab027c235b606b132
Mirza Abdullah Baig <abaig7921gmail.com>
2024-04-03 04:08:39 +0500

    Assignments_for_JS_Chapter-02

---
+++ Assignment Chapter 02/index.js
@@ -0,0 +1,39 @@
+let username = "abdullah";
+let myName = "Abdullah";
+let message = "Hello world";
+
+alert(myName);
+alert(username);
+alert(message);
+
+
+
+let student_name = "Usman";
+let student_age = "20 Year's Old";
+let student_course = "Certified Mobile Application Development";
+
+alert(student_name);
+alert(student_age);
+alert(student_course);
+
+
+let pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+
+alert(pizza);
+
+
+let email_sentence = "My email address is ";
+let email = "abaig7921@gmail.com";
+
+alert(email_sentence + email);
+
+
+let book = "A smarter way to learn JavaScript";
+alert(book);
+
+let sentence = "Yah! I can write HTML content through JavaScript";
+document.write(sentence);
+
+let design = "------&^&*@*#*#*#-------";
+
+alert(design);

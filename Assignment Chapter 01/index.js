7c7e3a6c9118838e91e1867e67ff1a193d5d2ce4
Mirza Abdullah Baig <abaig7921gmail.com>
2024-04-03 04:03:03 +0500

    Assignments_for_JS_Chapter-01

---
+++ Assignment Chapter 01/index.js
@@ -0,0 +1,11 @@
+let msg_1 = "Error! Plz enter a valid password.";
+let msg_2 = "Welcome to JS land...\nHappy coding!";
+let msg_3 = "Welcome to JS land...";
+let msg_4 = "Happy coding!";
+let msg_5 = "Hello... I can run JS through my web browser's console";
+
+alert(msg_1);
+alert(msg_2);
+alert(msg_3);
+alert(msg_4);
+alert(msg_5);

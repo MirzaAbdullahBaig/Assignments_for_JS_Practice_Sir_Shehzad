fa2aa9cef6e8ff8fdaad7640cca9532cf9045b05
Mirza Abdullah Baig <abaig7921gmail.com>
2024-04-13 07:14:55 +0500

    Assignments_for_JS_Chapter-03

---
+++ Assignment Chapter 03/index.js
@@ -0,0 +1,20 @@
+let age = prompt("Plz Enter Your Age");
+let sentence = `i am ${age} years old`;
+alert(sentence);
+
+let counter = 14;
+alert(`You visited this site ${counter} times`)
+
+let birthYear = 2003;
+let statement = `My Birth Year is ${birthYear}`
+let sec_statement = `Data type of my declared variable is ${typeof birthYear}`
+
+document.write(`${statement} <br/>`);
+document.write(`${sec_statement} <br/> <br/>`);
+
+let visitor_name = "Abdullah";
+let quantity = 5;
+let clothing_store = "XYZ Clothing Store";
+let product_title = `${visitor_name} ordered ${quantity} T-shirts on ${clothing_store}`;
+
+document.write(product_title);

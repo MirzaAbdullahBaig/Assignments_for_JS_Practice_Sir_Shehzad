38b9c31e93150c3de3411b01174ad64bd1eb6db2
Mirza Abdullah Baig <abaig7921gmail.com>
2024-04-14 20:14:16 +0500

    Assignments_for_JS_Chapter-04

---
+++ Assignment Chapter 04/index.js
@@ -0,0 +1,12 @@
+let heading = `Rules for naming JS Variable`;
+
+document.write(`${heading} <br/> <br/> <br/>`);
+
+let var_name = "Variable name can only contain letters, numbers, underscores and dollar. For example: $my_1stVariable";
+
+let var_name_begin = "Variable must begin with a letter, underscore and dollar. For example name, $name and _name";
+
+let var_rule_1 = "Variable names are case-insensitive"
+let var_rule_2 = "Variable names should not be JS keywords"
+
+document.write(`${var_name} <br/> ${var_name_begin} <br/> ${var_rule_1} <br/> ${var_rule_2}`)
